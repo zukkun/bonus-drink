950b9aff143cf89fbbd2e59cd5a0d09262ad0c7e
zukkun <zukkun@iwaboos-mba13.lan>
2014-10-12 18:48:55 +0900

    Implemented BonusDrink

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,16 @@
 class BonusDrink
+  BONUS_BOTTLES = 3
+
   def self.total_count_for(amount)
-    0
+    empty_bottles = amount
+    total_bottles = amount
+
+    while empty_bottles >= BONUS_BOTTLES do
+      new_bottles = empty_bottles / BONUS_BOTTLES
+      total_bottles += new_bottles
+      empty_bottles = empty_bottles % BONUS_BOTTLES + new_bottles
+    end
+
+    total_bottles
   end
 end
